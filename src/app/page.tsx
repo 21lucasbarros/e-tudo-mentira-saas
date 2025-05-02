4e34ba7a2b62625c4c0320b7ab00df8e9e611b36
Lucas Barros <lucasweacked21@gmail.com>
2025-05-02 02:59:58 -0300

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Hero from "@/components/hero";
 import HowItWorks from "@/components/how-it-works";
 import Newsletter from "@/components/newsletter";
 import Pricing from "@/components/pricing";
-import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
       <Header />
       <Hero />
       <HowItWorks />
-      <Testimonials />
       <Pricing />
       <Newsletter />
       <Footer />
